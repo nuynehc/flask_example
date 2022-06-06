e313192b08b8ae6b73b08658fdfa64aca5cbda31
nuynehc <nuynehc@users.noreply.github.com>
2022-06-06 14:51:24 +0000

    ✅ Ready to clone and code.

--- flask_example/app.py
+++ flask_example/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+flask_example API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="flask_example",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://flask_example.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "nuynehc",
+        "url": "http://flask_example.com/contact/",
+        "email": "nuynehc@flask_example.com",
     },
     license_info={
         "name": "The Unlicense",
